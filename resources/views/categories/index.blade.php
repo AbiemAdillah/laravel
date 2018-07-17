caacd4b4e626a09e19ef4a28795ac16506a93e62
Eko Wijaya <32318250+W17aya@users.noreply.github.com>
2018-07-17 14:15:18 +0800

    Edit

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -4,20 +4,20 @@
   <div class="container">
     <div class="row">
       <div class="col-md-12">
-        <h3>Category <small><a href="{{ route('categories.create') }}" class="btn btn-warning btn-sm">New Category</a></small></h3>
+        <h3>Kategori <small><a href="{{ route('categories.create') }}" class="btn btn-warning btn-sm">Kategori Baru</a></small></h3>
         {!! Form::open(['url' => 'categories', 'method'=>'get', 'class'=>'form-inline']) !!}
             <div class="form-group {!! $errors->has('q') ? 'has-error' : '' !!}">
-              {!! Form::text('q', isset($q) ? $q : null, ['class'=>'form-control', 'placeholder' => 'Type category...']) !!}
+              {!! Form::text('q', isset($q) ? $q : null, ['class'=>'form-control', 'placeholder' => 'Ketikkan kategori']) !!}
               {!! $errors->first('q', '<p class="help-block">:message</p>') !!}
             </div>
 
-          {!! Form::submit('Search', ['class'=>'btn btn-primary']) !!}
+          {!! Form::submit('Cari', ['class'=>'btn btn-primary']) !!}
         {!! Form::close() !!}
         <table class="table table-hover">
           <thead>
             <tr>
-              <td>Title</td>
-              <td>Parent</td>
+              <td>Nama</td>
+              <td>Jenis</td>
               <td></td>
             </tr>
           </thead>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,11 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Selamat Datang</div>
 
                 @if (Auth::guest())
                 <div class="panel-body">
-                    Please login to view catalog.
+                    Silahkan login Untuk Melihat Katalog.
                     <ul>
                         <li>Admin: admin@gmail.com | secret</li>
                         <li>Customer: customer@gmail.com | secret</li>
@@ -17,7 +17,7 @@
                 </div>
                 @else
                 <div class="panel-body">
-                    You're logged in.
+                    Kamu Telah Masuk.
                 </div>
                 @endif
             </div>
